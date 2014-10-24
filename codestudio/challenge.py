3c7b35ccdc2ad692245ad5ed23183f473b39a9c9
Rob Muhlestein <rob@skilstak.com>
2014-10-24 14:45:19 -0400

    refactored yaml config

--- codestudio/challenge.py
+++ codestudio/challenge.py
@@ -1,32 +1,33 @@
-import yaml
+from yamlconfig import YamlConfig
 from os import path
 here = path.abspath(path.dirname(__file__))
 files_path = path.join(here,'challenges')
 
-class YamlConfig():
-
-    def __str__(self): return str(self.__dict__)
+class ArtistChallenge():
+    '''
+    c = ArtistChallenge('s1level24')
+    c = ArtistChallenge(fname='s1level24')
+    c = ArtistChallenge('sllevel24','./challenges/s1level24.yaml')
+    c = ArtistChallenge(name='s1level24')
+    c = ArtistChallenge(fname='./challenges/s1level24.yaml')
+    c = ArtistChallenge(fname='./challenges/s1level24.yaml',name='s1level24')
+    '''
 
     def __init__(self,name=None,fname=None):
-        if name: self.load(name,fname)
-
-    def load(self,name,fname=None):
+        if not fname and name: fname = path.join(files_path,name+'.yaml')
+        if not name and fname: name = path.splitext(path.basename(fname))[0]
         self.name = name
-        if not fname:
-            fname = path.join(files_path,name+'.yaml')
-        with open(fname) as f:
-            self.config = yaml.load(f)
+        self.fname = fname
+        self.lines = []
+        if fname: self.load(fname)
 
-class ArtistChallenge():
-
-        def __init__(self,config=False,name=None):
-            self.lines = []
-            if not config and (name or fname):
-                self.config = YamlConfig(
-
-            for line in y['lines']:
+    def load(self,fname=None):
+        '''Loads an artist config (yaml) file'''
+        if fname: self.fname = fname
+        with YamlConfig(self.fname) as yaml:
+            for line in yaml.data['lines']:
                 self.lines.append(tuple([int(s) for s in line.split()]))
-            self.number_lines = len(self.lines)
+        self.number_lines = len(self.lines)
 
     def check(self,solution):
         if len(set(solution)) != self.number_lines: return False
@@ -40,10 +41,10 @@ if __name__ == '__main__':
 
     # TODO turn this into proper unittests later
 
-    #Challenge('s1level24').dump()
-    #Challenge('s1level24',fname='./challenges/s1level24.yaml').dump()
+    #ArtistChallenge('s1level24').dump()
+    #ArtistChallenge('s1level24',fname='./challenges/s1level24.yaml').dump()
 
-    c = Challenge('s1level24')
+    c = ArtistChallenge('s1level24')
     c.raise_exception = False
 
     # should fail
