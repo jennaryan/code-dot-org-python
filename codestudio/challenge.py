630c1160182e9181ac8f8b705082151b9bb42948
Rob Muhlestein <rob@skilstak.com>
2014-10-24 11:47:51 -0400

    lines loading

--- codestudio/challenge.py
+++ codestudio/challenge.py
@@ -5,19 +5,25 @@ files_path = path.join(here,'challenges')
 
 class Challenge():
 
+    def __str__(self): return str(self.__dict__)
+
     def __init__(self,name=None,fname=None):
+        self.lines = []
         if name: self.load(name,fname)
 
     def load(self,name,fname=None):
         if not fname:
             fname = path.join(files_path,name+'.yaml')
         with open(fname) as f:
-            self.config = yaml.load(f)
+            y = yaml.load(f)
+            self.ref = y['ref']
+            for line in y['lines']:
+                self.lines.append(tuple(line.split()))
 
-    def dump(self):
-        print(self.config)
 
+# TODO turn this into proper unittests later
 if __name__ == '__main__':
-    Challenge('s1level24').dump()
-    Challenge('s1level24',fname='./challenges/s1level24.yaml').dump()
+    print(Challenge('s1level24'))
+    #Challenge('s1level24').dump()
+    #Challenge('s1level24',fname='./challenges/s1level24.yaml').dump()
         
