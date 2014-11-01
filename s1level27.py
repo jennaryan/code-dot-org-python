85ca212f05c6da3dfd0a043e5e7f1516353551d0
Rob Muhlestein <rob@skilstak.com>
2014-11-01 10:23:01 -0400

    tweeks

--- s1level27.py
+++ s1level27.py
@@ -2,7 +2,7 @@ import codestudio
 artist = codestudio.load('s1level27')
 
 for count in range(3):
-    artist.pen.color='random'
+    artist.pen.color = 'random'
     # TODO draw a side of triangle
     
 artist.check()
--- s1level28.py
+++ s1level28.py
@@ -2,7 +2,7 @@ import codestudio
 artist = codestudio.load('s1level28')
 
 for count in range(3):
-    # TODO make a side of a triangle
-# TODO make a square using a loop
+    # TODO make a side of a triangle and turn
+    pass
     
 artist.check()
