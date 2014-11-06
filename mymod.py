a6b5b67503e7d50374e157e649f98fd522d3dafc
Rob Muhlestein <rob@skilstak.com>
2014-11-06 14:15:06 -0500

    s1level61

--- mymod.py
+++ mymod.py
@@ -8,6 +8,16 @@ you call `import mymod` in your scripts.
 import codestudio
 
 class Zombie(codestudio.Artist):
+    """An Artist with a propensity for brains and drawing squares.
+
+    While class definitions look like function definitions they are different.
+    The parameter inside the parenthesis () is the parent class. This means
+    all Zombies are Artists and can do everything an Artist can do.
+
+    The `start_direction` and `speed` are special variables that goes with
+    all Zombies. These are called class or static attributes. An attribute
+    is a variable that goes with a class or the objects created from a class.
+    """
     start_direction = 90            # facing the east, or right of screen
     speed = 'slow'                  # it is a zombie after all
     color = 'red'                   # it is a zombie after all
---
+++ stage11-artist3/s1level61.py
@@ -0,0 +1,22 @@
+"""Stage 11: Puzzle 3 of 11
+
+Use repeat (for) loops to draw 3 squares of size 100, each 120 degrees
+apart. And do it in 3 different, random colors.
+
+Hint: Use your new Zombie class, which we have moved into `mymod.py`
+library module for you.
+
+"""
+
+import sys
+sys.path.append('..')
+
+import codestudio
+import mymod
+artist = codestudio.load('s1level61')
+zombie = mymod.Zombie(artist)
+
+zombie.color = zombie.random_color()
+zombie.draw_square(100)
+
+zombie.check()
