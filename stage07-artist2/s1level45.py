a53940c6f0f49bf5f6f235e232650e8757955d6c
Rob Muhlestein <rob@skilstak.com>
2014-11-06 09:15:24 -0500

    s1level45

--- stage07-artist2/s1level45.py
+++ stage07-artist2/s1level45.py
@@ -16,7 +16,7 @@ artist.speed = 'fastest'
 # TODO alter or replace this code to draw whatever you want
 for count3 in range(18):
     for count2 in range(10):
-        artist.pen.color = 'random'
+        artist.color = 'random'
         for count in range(4):
             artist.move_forward(20)
             artist.turn_right(90)
