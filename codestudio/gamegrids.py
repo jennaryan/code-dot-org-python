06d7d18e8a5d7713d08035c903484e7130deadfa
Rob Muhlestein <rob@skilstak.com>
2014-11-15 08:02:38 -0500

    gamegrids golden

--- codestudio/gamegrids.py
+++ codestudio/gamegrids.py
@@ -1,4 +1,5 @@
 from collections import UserList
+import math
 
 class XYGrid(UserList):
     """Traditional coordinate game grid.
@@ -16,12 +17,12 @@ class XYGrid(UserList):
         self.xsize = xsize
         self.ysize = ysize
         self.data = [[value for y in range(ysize+1)] for x in range(xsize+1)]
-        for n in range(xsize):
-            self.data[n][0] = ''
-        for n in range(ysize):
-            self.data[0][n] = ''
+        for x in range(xsize+1):
+            self.data[x][0] = ''
+        for y in range(ysize+1):
+            self.data[0][y] = ''
 
-    def __str__(self):
+    def to_text(self):
         d = self.data
         string = ''
         for y in range(self.ysize):
@@ -29,3 +30,41 @@ class XYGrid(UserList):
                 string += d[x+1][y+1] + ' '
             string += "\n"
         return string
+
+    def draw_line(self,line,value=1):
+        (x1,y1,x2,y2) = line
+        angle = bearing(line)
+        self.data[x1][y1] = value
+        self.data[x2][y2] = value
+        for i in range(round(length(line))):
+            (dx,dy) = xy(x1,y1,angle,i)
+            self.data[round(dx)][round(dy)] = value
+
+def bearing(line):
+    angle = math.degrees(math.atan2(line[3]-line[1],line[2]-line[0]))
+    if 0 <= angle <= 90:
+        return 90 - angle
+    elif 90 < angle <= 180:
+        return 180 - angle + 270 
+    elif 0 > angle >= -90:
+        return 90 - angle
+    elif -90 > angle > -180:
+        return 90 - angle
+
+def length(line):
+    """Thank you Pythagoras. You would have loved Python."""
+    return math.sqrt((line[2] - line[0]) ** 2 + (line[3] - line[1]) ** 2)
+
+def xy(x=0, y=0, direction=0, amount=0):
+    """Returns a new (x,y) coordinate after adding the amount in
+    the given direction."""
+    angle = math.radians(direction)
+    return (math.sin(angle) * amount + x, math.cos(angle) * amount + y)
+
+class XYZGrid(UserList):
+    """Adds 3rd dimension mostly for layering on XYGrids"""
+    pass
+
+class XYBitGrid(UserList):
+    """Ultra fast XY grid as packed struct composed of boolean on/offs"""
+    pass
--- tests/gamegrids_tests.py
+++ tests/gamegrids_tests.py
@@ -1,14 +1,62 @@
 import sys
+import math
 sys.path.append('..')
-import codestudio.gamegrids as g
-from nose.tools import assert_equals
+import codestudio.gamegrids as gg
+from nose.tools import eq_
 
-def Grid_test():
-    grid = g.XYGrid()
-    assert_equals(grid,[[]])
-    grid.init(3,2,'~')
-    assert_equals(str(grid),"~ ~ ~ \n~ ~ ~ \n")
-    grid[1][1] = '*'
-    assert_equals(str(grid),"* ~ ~ \n~ ~ ~ \n")
-    assert False
+class TestXYGrid():
 
+    def setup(self):
+        self.grid = gg.XYGrid()
+
+    def defaults_test(self):
+        eq_(self.grid,[[]])
+
+    def init_test(self):
+        self.grid.init(3,2,'~')
+        eq_(self.grid,[['','',''],['','~','~',],['','~','~'],['','~','~']])
+        self.grid[1][1] = '*'
+        eq_(self.grid,[['','',''],['','*','~',],['','~','~'],['','~','~']])
+        self.grid.init(10,10,0)
+        self.grid[4][7] = '*'
+        tenxten = [
+            ['', '', '', '', '', '', '', '', '', '', ''],
+            ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['', 0, 0, 0, 0, 0, 0, '*', 0, 0, 0],
+            ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        eq_(self.grid,tenxten)
+
+    def to_text_test(self):
+        self.grid.init(3,2,'~')
+        self.grid[1][1] = '*'
+        eq_(self.grid.to_text(),'* ~ ~ \n~ ~ ~ \n')
+
+    def draw_line_test(self):
+        self.grid.init(10,10,'~')
+        self.grid.draw_line((1,1,4,4),'*')
+        string = ("* ~ ~ ~ ~ ~ ~ ~ ~ ~ \n"
+                  "~ * ~ ~ ~ ~ ~ ~ ~ ~ \n"
+                  "~ ~ * ~ ~ ~ ~ ~ ~ ~ \n"
+                  "~ ~ ~ * ~ ~ ~ ~ ~ ~ \n"
+                  "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ \n"
+                  "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ \n"
+                  "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ \n"
+                  "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ \n"
+                  "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ \n"
+                  "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ \n")
+        eq_(self.grid.to_text(),string)
+
+def length_test():
+    eq_(gg.length((0,0,1,3)),math.sqrt((1-2)**2+(3-0)**2))
+
+def xy_test():
+    end = tuple(round(n) for n in gg.xy(1,3,30,2))
+    eq_(end,(2,5))
